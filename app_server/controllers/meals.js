955c7abc247c60c6804cbf5a6f90c41df1521d9b
Austin Fuchs <austin.fuchs@snhu.edu>
2022-07-14 23:56:00 -0400

    Coverted static travel, rooms, and meals templates to dynamic JSON

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const mealsData = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 /* GET meals view */
 const meals = (req, res) => {
-    res.render('meals', { title: 'Travlr Getaways - Meals' });
+    res.render('meals', { title: 'Travlr Getaways - Meals', mealsData});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Travlr Getaways - Rooms' });
+    res.render('rooms', { title: 'Travlr Getaways - Rooms' , roomsData});
 };
 
 module.exports = {
